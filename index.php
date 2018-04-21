438516480b0539718dfbbcd97129df57cf5f51a8
Oscar Barraza <oscarbarraza@dhcp-rhodes-1413.eduroam.cornell.edu>
2018-04-20 22:06:46 -0400

    added dashboard and included the d3 library

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 	  <meta name="viewport" content="width=device-width, initial-scale=1" />
 	  <link rel="stylesheet" type="text/css" href="styles/master.css" media="all"/>
 		<link href="https://fonts.googleapis.com/css?family=Playfair+Display|Source+Sans+Pro:200" rel="stylesheet">
+		<script src="https://d3js.org/d3.v5.min.js"></script>
 
 	  <title>Chef's Choice</title>
 	</header>
